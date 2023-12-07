073d7f75a09d03544c46302464c1d107180b1c9f
AYAABDELNABI <aya.abdelnabi.sw@gmail.com>
2023-12-07 13:44:54 +0200

    hh

--- python-import_ module/0-add.py
+++ python-import_ module/0-add.py
@@ -1,13 +1,12 @@
-from add_0 import add
 
-def main():
-    global a, b
-    a = 1
-    b = 2
-    result = add(a, b)
-    print("{} + {} = {}".format(a, b, result))
 
-if __name__ == "__main__":
-    main()
+a = 1
+b = 2
+ 
+from add_0 import add  
+
+result = add(a, b)
+
+print("{} + {} = {}".format(a, b, result))
 
 
