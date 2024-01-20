d0e54861a7aa43e80a2fb344f06e016678c22a91
AYAABDELNABI <aya.abdelnabi.sw@gmail.com>
2024-01-20 22:54:41 +0200

    AA

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,18 +1,14 @@
-"""
-A script that starts a Flask web application:
-listening on 0.0.0.0, port 5000
-Routes: / display “Hello HBNB!”
+"""a script that starts a Flask web application:
 """
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_HBNB():
-    """
-    A function that returns specified string when routing to /
-    """
+
+@app.route("/", strict_slashes=False)
+def hello():
     return "Hello HBNB!"
 
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0",port="5000")
+    app.run(host="0.0.0.0", port=5000)
