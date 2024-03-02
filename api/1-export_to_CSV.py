fb798e5f4db6929154941f5e86e09cd35b1663ac
AYA ABDELNABI <aya.abdelnabi.sw@gmail.com>
2024-03-02 02:42:56 +0200

    AA

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,15 +1,25 @@
 import csv
-import requests 
-from sys import argv
+import requests
+import sys
 
-id = argv[1]
+if len(sys.argv) != 2:
+    print("Please provide an ID as an argument.")
+    sys.exit(1)
+
+id = sys.argv[1]
 url1 = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
-empurl= f'https://jsonplaceholder.typicode.com/users/{id}'
+empurl = f'https://jsonplaceholder.typicode.com/users/{id}'
 
 res1 = requests.get(url1)
+if res1.status_code != 200:
+    print(f"Error: Unable to fetch data from {url1}")
+    sys.exit(1)
 data1 = res1.json()
 
 res2 = requests.get(empurl)
+if res2.status_code != 200:
+    print(f"Error: Unable to fetch data from {empurl}")
+    sys.exit(1)
 employeedata = res2.json()
 
 USER_ID = employeedata['id']
@@ -20,7 +30,8 @@ TASK_TITLE = ''
 
 with open(f'{USER_ID}.csv', 'w', newline='') as file:
     writer = csv.writer(file)
+    writer.writerow(['User ID', 'Username', 'Task Completed Status', 'Task Title'])
     for i in range(len(data1)):
         TASK_COMPLETED_STATUS = data1[i]['completed']
         TASK_TITLE = data1[i]['title']
-        writer.writerow([USER_ID,USERNAME,TASK_COMPLETED_STATUS,TASK_TITLE])
+        writer.writerow([USER_ID, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE])
