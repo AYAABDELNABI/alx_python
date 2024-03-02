670d42a59dace03f893bdd1ad6c7da70b83cb15b
AYA ABDELNABI <aya.abdelnabi.sw@gmail.com>
2024-03-02 02:20:32 +0200

    AA

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,20 +1,26 @@
 import csv
+import requests 
+from sys import argv
 
-def export_tasks_to_csv(user_id, username, tasks):
-    filename = f"{user_id}.csv"
-    with open(filename, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for task in tasks:
-            writer.writerow([user_id, username, task["completed"], task["title"]])
+id = argv[1]
+url1 = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
+empurl= f'https://jsonplaceholder.typicode.com/users/{id}'
 
-# Example data
-user_id = "123"
-username = "example_user"
-tasks = [
-    {"title": "Task 1", "completed": True},
-    {"title": "Task 2", "completed": False},
-    {"title": "Task 3", "completed": True}
-]
+res1 = requests.get(url1)
+data1 = res1.json()
 
-export_tasks_to_csv(user_id, username, tasks)
+res2 = requests.get(empurl)
+employeedata = res2.json()
+
+USER_ID = employeedata['id']
+USERNAME = employeedata['username']
+TASK_COMPLETED_STATUS = ''
+TOTAL_NUMBER_OF_TASKS = len(data1)
+TASK_TITLE = ''
+
+with open(f'{USER_ID}.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    for i in range(len(data1)):
+        TASK_COMPLETED_STATUS = data1[i]['completed']
+        TASK_TITLE = data1[i]['title']
+        writer.writerow([USER_ID,USERNAME,TASK_COMPLETED_STATUS,TASK_TITLE])
