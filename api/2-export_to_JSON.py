76df56094af3f80951b52fdbc293e57d9fd11383
AYA ABDELNABI <aya.abdelnabi.sw@gmail.com>
2024-03-02 02:33:28 +0200

    AA

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,5 +1,6 @@
-"""This module makes 2 api requests and 
-uses the informatiom given to write it to a json file
+"""
+This module makes 2 API requests and 
+uses the information given to write it to a JSON file
 """
 
 import json
@@ -9,34 +10,28 @@ from sys import argv
 def get_info(id):
     url1 = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
     empurl= f'https://jsonplaceholder.typicode.com/users/{id}'
-    # get todo tasks
+    # Get todo tasks
     res1 = requests.get(url1)
     data1 = res1.json()
-    # get employee information
+    # Get employee information
     res2 = requests.get(empurl)
     employeedata = res2.json()
 
-    """Requred data"""
     USER_ID = employeedata['id']
     USERNAME = employeedata['username']
-    TASK_COMPLETED_STATUS = ''
     TOTAL_NUMBER_OF_TASKS = len(data1)
-    TASK_TITLE = ''
     tasks = []
-    for i in range(len(data1)):
-        """loop to add task status and title"""
-        TASK_COMPLETED_STATUS = data1[i]['completed']
-        TASK_TITLE = data1[i]['title']
-        tasks.append({"task": TASK_TITLE, "completed": TASK_COMPLETED_STATUS, "username":USERNAME})
+    for task in data1:
+        tasks.append({"task": task['title'], "completed": task['completed'], "username": USERNAME})
         
-    dictionary={USER_ID: tasks}
-    with open(f'{id}.json', 'w', newline='') as file:
-        # write dictionary to json file
-        json.dump(dictionary , file)
+    dictionary = {USER_ID: tasks}
+    with open(f'{id}.json', 'w') as file:
+        json.dump(dictionary, file, indent=4)
 
 if __name__ == "__main__":
-    # if argv[1]:
-        get_info(int(argv[1]))
-    # else:
-        for i in range(1,11):
+    if len(argv) > 1:
+        for arg in argv[1:]:
+            get_info(int(arg))
+    else:
+        for i in range(1, 11):
             get_info(i)
